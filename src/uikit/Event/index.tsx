7e4aa57352c2b7cf14fdd81f98b7495dec83a937
programist78 <pavelmolodecfda@gmail.com>
2023-09-18 13:44:07 +0300

    Update index.tsx

--- src/uikit/Event/index.tsx
+++ src/uikit/Event/index.tsx
@@ -4,6 +4,10 @@ import { EventProps } from "./interface"
 import styles from './Event.module.scss'
 import { useTranslations } from "next-intl"
 
+const imageLoader = ({ url, width, quality }: {url: String, width: Number, quality: Number}) => {
+    return `${url}?w=${width}&q=${quality || 75}`
+  }
+
 export const Event = ({
     url,
     title,
@@ -11,20 +15,20 @@ export const Event = ({
     onClick,
 }: EventProps) => {
     const t = useTranslations('Global')
+    let width = 200
+    let quality = 80
 
+       
     return (
         <div className={styles.event}>
-            {/* <Image
-                alt=''
-                src={url}
-                width={295}
-                height={297}
-                onError={(e) => {
-                    e.target.onerror = null; // Отключаем обработчик ошибки, чтобы избежать бесконечных вызовов
-                    e.target.src = '/images/eventImg1.webp' // Путь к изображению по умолчанию
-                }}
-                className={styles.image}
-            /> */}
+    <Image 
+    className={styles.image}
+        width={50}
+        height={50}
+        src={url}
+        loader={imageLoader}
+        alt={title}
+    />
             <p className={styles.title}>{title}</p>
             <p className={styles.description}>{description}</p>
             <div className={styles.button} onClick={onClick}>
